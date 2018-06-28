9ba4670f6fca258e399d75f338998e5896bc92e3
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-06-28 22:54:57 +0900

    update

---
+++ RobotCar01/RobotCar.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pigpio
+import CR_Servo
+
+import os
+import sys
+import time
+import csv
+
+#####
+class RobotCar:
+    DEF_CONF_FILENAME = 'robot_car.csv'
+    #DEF_CONF_FILE = os.environ['HOME']+'/'+DEF_CONF_FILENAME
+    DEF_CONF_FILE = './'+DEF_CONF_FILENAME
+
+    DEF_PULSE_VAL = { 'off':      [0,0], \
+                      'stop':     [1480,1480], \
+                      'break':    [1480,1480], \
+                      'forward':  [1580,1380], \
+                      'backward': [1380,1580], \
+                      'left':     [1580,1580], \
+                      'right':    [1380,1380]   }
+
+    def __init__(self, pin, conf_file=''):
+        self.pi = pigpio.pi()
+        self.pin = pin
+        self.n = len(pin)
+
+        self.cr_servo = CR_Servo.CR_Servo_N(self.pi, self.pin)
+
+        self.pulse_val = RobotCar.DEF_PULSE_VAL
+
+        self.conf_file = conf_file
+        if self.conf_file == '':
+            self.conf_file = RobotCar.DEF_CONF_FILE
+        self.conf_load(self.conf_file)
+
+        self.move('stop')
+
+    def __del__(self):
+        print('=== RobotCar self.__del__() ===')
+        print('RobotCar self.move(\'stop\')')
+        self.move('stop')
+        print('RobotCar self.move(\'off\')')
+        self.move('off')
+        print('RobotCar self.pi.stop()', end='')
+        self.pi.stop()
+        print('.')
+       
+    ###
+    def move(self, key, sec=0.0):
+        #print(key)
+        self.set_pulse(self.pulse_val[key], sec)
+
+    ###
+    def set_pulse(self, pulse, sec=0.0):
+        self.cr_servo.set_pulse(pulse, sec)
+
+    def increment_pulse_val(self, key, idx, d_val):
+        self.pulse_val[key][idx] += d_val
+        #print(key, self.pulse_val[key][idx])
+        self.conf_save(self.conf_file)
+        
+    def change_pulse_val(self, key, val):
+        self.pulse_val[key] = val
+        self.conf_save(self.conf_file)
+
+    def conf_load(self, conf_file=''):
+        if conf_file == '':
+            conf_file = RobotCar.DEF_CONF_FILE
+        #print('conf_file =', conf_file)
+
+        with open(conf_file, 'r', encoding='utf-8') as f:
+            csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                #print(row)
+                if len(row) < len(self.pulse_val['stop']):
+                    #print('empty')
+                    continue
+                if row[0][0:1] == '#':
+                    #print('ignore')
+                    continue
+
+                self.pulse_val[row[0].lower()] = [int(row[1]), int(row[2])]
+
+        #print(self.pulse_val)
+
+    def conf_save(self, conf_file=''):
+        if conf_file == '':
+            conf_file = RobotCar.DEF_CONF_FILE
+        #print('conf_file =', conf_file)
+
+        with open(conf_file, 'w', encoding='utf-8') as f:
+            csv_writer = csv.writer(f, lineterminator='\n')
+            csv_writer.writerow(['#Key', 'Left Motor', 'Right Motor'])
+
+            for k in self.pulse_val.keys():
+                #print(k, self.pulse_val[k])
+                csv_writer.writerow([k, \
+                                     self.pulse_val[k][0], \
+                                     self.pulse_val[k][1]])
+
+
+#####
+def main():
+    pin = [12, 13]
+
+    robot = RobotCar(pin)
+
+    robot.conf_save()
+    
+    robot.move('forward', 0.1)
+    robot.move('stop', 1)
+    robot.increment_pulse_val('stop', 0, +10)
+    robot.move('backward', 0.1)
+    robot.move('stop', 1)
+    robot.increment_pulse_val('stop', 0, -10)
+    robot.move('left', 0.5)
+    robot.move('stop', 1)
+    robot.move('right', 1)
+    robot.move('stop', 1)
+    robot.move('off')
+
+if __name__ == '__main__':
+    main()
---
+++ RobotCar01/robot_car.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import RobotCar
+import VL53L0X
+import threading
+import queue
+import readchar
+import time
+
+PIN_CR_SERVO = [13, 12]
+
+CmdQ = queue.Queue()
+
+Tof = None
+TofTiming = 0
+
+DISTANCE_NEAR = 300	# mm
+DISTANCE_NEAR2 = 100	# mm
+
+#####
+def get_distance():
+    distance = Tof.get_distance()
+    print('distance: %d mm' % distance, '\r')
+    return distance
+
+#####
+def robot_auto(myid, robot):
+    print(myid, 'robot_auto():start', '\r')
+
+    last_turn = 'left'
+    forward_count = 0
+    FORWARD_COUNT_MAX = 10
+
+    robot.move('stop')
+    
+    while True:
+        if not CmdQ.empty():
+            robot.move('stop', 0.1)
+            break
+
+        distance = get_distance()
+
+        if distance > DISTANCE_NEAR and forward_count < FORWARD_COUNT_MAX:
+            robot.move('forward', 0.1)
+            forward_count += 1
+            print('forward_count =', forward_count, '\r')
+            continue
+
+        if forward_count >= FORWARD_COUNT_MAX:
+            if forward_count > 0:
+                if last_turn == 'left':
+                    last_turn = 'right'
+                else:
+                    last_turn = 'left'
+                    
+            robot.move(last_turn, 0.3)
+            forward_count = 0
+            continue
+
+        ## Near
+        robot.move('stop', 1)
+        distance = get_distance()
+        
+        forward_count = 0
+
+        while distance < DISTANCE_NEAR:
+            print('!', '\r')
+            if distance < DISTANCE_NEAR2:
+                print('!!', '\r')
+                robot.move('backward', 0.3)
+                if last_turn == 'left':
+                    last_turn = 'right'
+                else:
+                    last_turn = 'left'
+                robot.move(last_turn, 1.0)
+
+                robot.move('stop', 1)
+                distance = get_distance()
+                continue
+            
+            if last_turn == 'left':
+                robot.move('right', 0.3)
+            else:
+                robot.move('left', 0.3)
+            robot.move('stop', 1)
+            distance = get_distance()
+
+        if last_turn == 'left':
+            last_turn = 'right'
+        else:
+            last_turn = 'left'
+        print('last_turn =', last_turn, '\r')
+            
+
+    print(myid, 'robot_auto():end', '\r')
+
+        
+def robot_thread():
+    myid = "robot_thread()"
+    print(myid, 'start', '\r')
+
+    robot = RobotCar.RobotCar(PIN_CR_SERVO)
+
+    [idx_left, idx_right] = [0, 1]
+
+    move_cmd = {\
+                's':'stop', \
+                'S':'break', \
+                'w':'forward', \
+                'x':'backward', \
+                'a':'left', \
+                'd':'right' \
+    }
+
+    move_stat = 'stop'
+    robot.move('stop')
+    
+    while True:
+        cmd = CmdQ.get()
+        print('distance: %d mm' % get_distance(), '\r')
+
+        if len(cmd) > 0:
+            print(myid, '"'+cmd+'"', '\r')
+
+        if cmd == ' ' or ord(cmd) < 20:
+            robot.move('off')
+            break
+
+        ###
+        if cmd == '@':
+            robot_auto(myid, robot)
+
+        if cmd == 'z':
+            robot.increment_pulse_val(move_stat, idx_left, -5)
+            robot.move(move_stat)
+        if cmd == 'q':
+            robot.increment_pulse_val(move_stat, idx_left, 5)
+            robot.move(move_stat)
+        if cmd == 'e':
+            robot.increment_pulse_val(move_stat, idx_right, -5)
+            robot.move(move_stat)
+        if cmd == 'c':
+            robot.increment_pulse_val(move_stat, idx_right, 5)
+            robot.move(move_stat)
+
+        ###
+        if cmd in move_cmd.keys():
+            move_stat = move_cmd[cmd]
+            print(myid, cmd, '->', move_stat, '\r')
+            robot.move(move_stat)
+
+    robot = None
+    print(myid, 'end', '\r')
+           
+#####
+def readchar_thread():
+    myid = 'readchar_thread()'
+    print(myid, 'start', '\r')
+
+    while True:
+        ch = readchar.readchar()
+        print(myid, '"'+ch+'"', '\r')
+
+        CmdQ.put(ch)
+
+        if ch == ' ' or ord(ch) <= 20:
+            break
+
+    print(myid, 'end', '\r')
+    
+#####
+def main():
+    global Tof
+    global TofTiming
+
+    Tof = VL53L0X.VL53L0X()
+    Tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+    TofTiming = Tof.get_timing()
+    if TofTiming < 20000:
+        TofTiming = 20000
+    print("TofTiming = %d ms" % (TofTiming/1000))
+    print('distance: %d mm' % get_distance())
+
+    robot_th = threading.Thread(target=robot_thread, daemon=True)
+    robot_th.start()
+
+    readchar_th = threading.Thread(target=readchar_thread, daemon=True)
+    readchar_th.start()
+
+    
+    #CmdQ.join()
+    readchar_th.join()
+    print('join: readchar_th')
+    robot_th.join()
+    print('join: robot_th')
+    print('join done.')
+
+if __name__ == '__main__':
+    main()
